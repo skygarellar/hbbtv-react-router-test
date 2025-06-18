07ff9637297fc705c146cb839b44aa20f4975764
Roberto Garella <roberto.garella@gmail.com>
2025-06-18 10:52:11 +0200

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import legacy from "@vitejs/plugin-legacy";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    legacy({
+      targets: ["Chrome >= 36"],
+      renderModernChunks: false,
+      renderLegacyChunks: true,
+    }),
+  ],
+});
